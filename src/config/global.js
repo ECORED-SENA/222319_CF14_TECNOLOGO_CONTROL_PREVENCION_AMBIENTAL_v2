44e187f8c0cbf5d62f9217987af5fd12a182fc27
elrilacho <camilobr17@gmail.com>
2023-07-21 11:44:36 -0500

    material_menu

--- src/config/global.js
+++ src/config/global.js
@@ -126,11 +126,11 @@ export default {
         titulo: 'Descargar PDF',
         download: 'downloads/222319_CF14_DU.zip',
       },
-      // {
-      //   icono: 'fas fa-download',
-      //   titulo: 'Descargar HTML',
-      //   download: 'downloads/material.zip',
-      // },
+      {
+        icono: 'fas fa-download',
+        titulo: 'Descargar HTML',
+        download: 'downloads/material.zip',
+      },
       {
         icono: 'far fa-registered',
         titulo: 'Créditos',
